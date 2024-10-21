032f2a6a3b3da5091fdb573738972cf16aeed238
Matthijs Kuiper <info@matthijskuiper.nl>
2024-10-21 16:04:40 +0200

    Use fetch for loading data

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,7 @@
 // TODO:
-// - Highlight MPCs the moment they are discovered
 // - Draw orbits (for planets)
-// - Controls: speed, etc.
-// - Add 3D/Anaglyph mode
 // - Create layers: planets above asteroids
+// - Support conpact JSON data for asteroids
 //
 // Inspiration:
 // https://www.youtube.com/watch?v=BKKg4lZ_o-Y
@@ -13,7 +11,6 @@
 // http://mgvez.github.io/jsorrery/
 // https://lord.io/blog/2014/kepler/
 
-import { ajaxGet } from "./utils";
 import Orrery from "./Orrery.js";
 import planetData from "./planets.js";
 import catalog from "../../data/catalog.json";
@@ -24,21 +21,21 @@ const MPC_DATA_URL = catalog;
 // Init Orrery
 const orrery = new Orrery({
   width: window.innerWidth,
-  height: window.innerHeight
+  height: window.innerHeight,
 });
 
 // Add planets
 orrery.addPlanets(planetData);
 
 // Load asteroids
-ajaxGet(MPC_DATA_URL, data => {
-  const asteroidData = JSON.parse(data);
+fetch(MPC_DATA_URL)
+  .then((response) => response.json())
+  .then((data) => {
+    // Sort by discovery date
+    data.sort((a, b) => a.disc - b.disc);
 
-  // Sort by discovery date
-  asteroidData.sort((a, b) => a.disc - b.disc);
-
-  orrery.setAsteroids(asteroidData);
-});
+    orrery.setAsteroids(data);
+  });
 
 // Resize orrery
 window.addEventListener("resize", () => {
